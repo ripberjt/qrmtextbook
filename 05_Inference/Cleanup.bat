9cc7eb16b5bf39afe51acec23e67690a2b004c60
joe ripberger <jtr@ou.edu>
2017-09-08 14:44:22 -0500

    Remove cleanup .bat files
    Remove cleanup .bat files

--- 05_Inference/Cleanup.bat
+++
@@ -1,8 +0,0 @@
-del *.aux
-del *.log
-del *.gz
-del *.lof
-del *.out
-del *.toc
-del *.lot
-pause
--- 14_Topics/Cleanup.bat
+++
@@ -1,9 +0,0 @@
-del *.aux
-del *.log
-del *.gz
-del *.lof
-del *.out
-del *.toc
-del *.lot
-del *concordance.tex
-pause
